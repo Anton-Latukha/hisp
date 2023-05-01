ea553d94bf6605cad929d70b1128ba22a04fef89
Anton Latukha <anton.latukha@gmail.com>
2023-05-01 20:33:06 +0300

    Hisp: add demo run it under main

--- src/Hisp.hs
+++ src/Hisp.hs
@@ -1,7 +1,8 @@
 {-# language TypeFamilies #-}
+{-# language UndecidableInstances #-}
 
 module Hisp
-  (
+  ( main
   )
 where
 
@@ -14,7 +15,7 @@ import qualified Data.Text                     as Text
 import qualified Data.Text.Read                as Text
 
 newtype Name = Name Text
- deriving (Eq, Show, Hashable)
+ deriving (Eq, Show, Hashable, IsString)
 
 data Expr
   = Var Name
@@ -23,28 +24,49 @@ data Expr
  deriving Show
 
 data Value
-  = Closure Env Name Expr
+  = Closure (Env Value) Name Expr
  deriving Show
 
-newtype Env = Env (HashMap Name Value)
- deriving (Show, Semigroup, Monoid)
+newtype Env a = Env (HashMap Name a)
+ deriving (Show, IsList, Semigroup, Monoid, Functor, Foldable, Traversable)
 
-instance One Env where
-  type OneItem Env = (Name, Value)
-  one (x, y) = Env $ one (x, y)
+instance One (Env a) where
+  type OneItem (Env a) = (Name, a)
+  one :: OneItem (Env a) -> Env a
+  one = coerce . one @(HashMap Name a)
 
-eval :: MonadFail m => Env -> Expr -> m Value
-eval e (Var x) =
-  maybe
-    (fail "bad")
-    pure
-    $ M.lookup x $ coerce e
-eval e (App f x) =
-  do
-    fv <- eval e f
-    xv <- eval e x
-    apply fv xv
-eval e (Lambda n x) = pure $ Closure e n x
+eval :: MonadFail m => Env Value -> Expr -> m Value
+eval e =
+  \case
+    Var x     ->
+      maybe
+        (fail "bad")
+        pure
+        $ M.lookup x $ coerce e
+    App f x    ->
+      join $ liftA2
+        apply
+          (eval e f)
+          (eval e x)
+    Lambda n x ->
+      pure $ Closure e n x
+
+data Tar
+  = Fresh
+  | Wrap Expr
 
 apply :: MonadFail m => Value -> Value -> m Value
 apply (Closure e n x) v = eval (one (n, v) <> e) x
+
+program :: MonadFail m => Env Expr -> m (Env Value)
+program = traverse (eval mempty)
+
+example :: MonadFail m => m Value
+example =
+  do
+    e <- program $
+      fromList [("id", Lambda "x" $ Var "x"), ("const", Lambda "x" $ Lambda "y" $ Var "x")]
+    eval e $ App (Var "const") (Var "id")
+
+main :: IO ()
+main = print =<< example
