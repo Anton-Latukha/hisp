33d6097fe3689d0d3ce65daf4f24d9ff2545642b
Anton Latukha <anton.latukha@gmail.com>
2023-05-04 19:43:40 +0300

    Hisp: implement the ugly `fresh` way

--- src/Hisp.hs
+++ src/Hisp.hs
@@ -25,6 +25,11 @@ data Expr
 
 data Value
   = Closure (Env Value) Name Expr
+  | Neutral Neutral
+ deriving Show
+
+newtype Neutral
+  = NVar Name
  deriving Show
 
 newtype Env a = Env (HashMap Name a)
@@ -36,38 +41,52 @@ instance One (Env a) where
   one = coerce . one @(HashMap Name a)
 
 eval :: MonadFail m => Env Value -> Expr -> m Value
-eval e =
+eval env =
   \case
     Var x     ->
       maybe
         (fail "bad")
         pure
-        $ M.lookup x $ coerce e
+        $ M.lookup x $ coerce env
     App f x    ->
       join $ liftA2
         apply
-          (eval e f)
-          (eval e x)
+          (eval env f)
+          (eval env x)
     Lambda n x ->
-      pure $ Closure e n x
+      pure $ Closure env n x
 
-data Tar
-  = Fresh
-  | Wrap Expr
+type Namespace = [Name]
+
+--  2023-05-04: FIXME: Find a way to remove this aglines, maybe with type abstraction.
+fresh :: Namespace -> Name -> Name
+fresh xs x
+  bool
+    x
+    (fresh xs (x <> "'"))
+    (x `elem` xs)
 
 -- | A funny name for a function that turns value back into readable expression.
-valueExpress :: MonadFail m => Value -> m Expr
-valueExpress =
-  undefined
+valueExpress :: MonadFail m => Namespace -> Value -> m Expr
+valueExpress xs (Closure env x b) =
+  do
+    let
+      x' = fresh xs x
+    bv <- eval (one (x, Neutral $ NVar x') <> env) b
+    Lambda x' <$> valueExpress (one x' <> xs) bv
+valueExpress _ (Neutral n) = valueExpressNeutral n
+
+valueExpressNeutral :: MonadFail m => Neutral -> m Expr
+valueExpressNeutral (NVar x) = pure $ Var x
 
 nf :: MonadFail m => Env Value -> Expr -> m Expr
-nf e t =
+nf env t =
   do
-    v <- eval e t
+    v <- eval env t
     valueExpress v
 
 apply :: MonadFail m => Value -> Value -> m Value
-apply (Closure e n x) v = eval (one (n, v) <> e) x
+apply (Closure env n x) v = eval (one (n, v) <> env) x
 
 -- | Builds the environment,
 -- embodies which variables and functions are in the scope of something.
@@ -77,15 +96,15 @@ scope = traverse (eval mempty)
 example :: MonadFail m => m Value
 example =
   do
-    e <- scope $
+    env <- scope $
       fromList [("id", Lambda "x" $ Var "x"), ("const", Lambda "x" $ Lambda "y" $ Var "x")]
-    eval e $ App (Var "const") (Var "id")
+    eval env $ App (Var "const") (Var "id")
 
 yCombinator :: MonadFail m => m Value
 yCombinator =
   do
-   e <- scope $ fromList $ one y'
-   eval e $ Var "yCombinator"
+   env <- scope $ fromList $ one y'
+   eval env $ Var "yCombinator"
  where
   x' =
     Lambda "x" $
